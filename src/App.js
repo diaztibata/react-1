f6e40eebe736a19bb1781a899378e9ac2e9d1710
diaztibata <diaztibata@gmail.com>
2022-11-25 12:30:48 -0500

    primeros pasos react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const todos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el cursso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LALALALAA', completed: false },
+];
+
+function App(props) {
+  return (
+    <h1>tio</h1>
+  );
+}
+
+export {App};
